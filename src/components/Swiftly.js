a631660e4cd1097ffba7c4df0617140d538f3372
Katharine Zarbock <kjzarbock@gmail.com>
2023-06-22 13:57:52 -0500

    6/28/23 1644, fixed names

--- src/components/Swiftly.js
+++ src/components/Swiftly.js
@@ -4,10 +4,10 @@ import { ApplicationViews } from "./views/ApplicationViews"
 import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
-import "./Repairs.css"
+import "./Swiftly.css"
 
 
-export const Repairs = () => {
+export const Swiftly = () => {
 	return <Routes>
 		<Route path="/login" element={<Login />} />
 		<Route path="/register" element={<Register />} />
